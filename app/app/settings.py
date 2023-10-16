7af84900226b99523f0fff89a54244d165fd0363
Zegg323 <shadow.enrique@hotmail.com>
2023-10-15 19:42:31 -0600

    add index.html

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    #"bootstrap5",
+    #'django_jinja',
     # 'django_extensions',
 ]
 
@@ -67,6 +69,7 @@ TEMPLATES = [
             ],
         },
     },
+
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
@@ -122,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'website/statics')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
